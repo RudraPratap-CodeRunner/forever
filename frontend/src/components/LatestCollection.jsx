195d1782fe091b24218c73ff1da4f4519d556692
RudraPratap-CodeRunner <rp.rudrapratapofficial@gmail.com>
2024-08-28 22:10:47 +0530

    BestSeller and NewsletterBox section added

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -7,7 +7,7 @@ const LatestCollection = () => {
     const {products} = useContext(ShopContext);
     const [latestProducts,setLatestProducts] = useState([]);
     useEffect(()=>{
-        setLatestProducts(products.slice(0,10))
+        setLatestProducts(products.slice(0,8))
     },[])
   return (
     <div className='my-10'>
@@ -15,7 +15,7 @@ const LatestCollection = () => {
             <Title text1={'LATEST'} text2={'COLLECTION'} />
             <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, molestiae.</p>
         </div>
-        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4  gap-4 gap-y-6'>
             {
                 latestProducts.map((item,index)=>(
                     <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price} />
--- frontend/src/components/ProductItem.jsx
+++ frontend/src/components/ProductItem.jsx
@@ -6,8 +6,8 @@ const ProductItem = ({id,image,name,price}) => {
     const {currency} = useContext(ShopContext);
   return (
     <Link to={`/product/${id}`} className='text-gray-700 cursor-pointer'>
-        <div className='overflow-hidden'>
-            <img className='hover:scale-110 transition ease-in-out' src={image[0]} alt="" />
+        <div className='overflow-hidden hover:scale-[1.05] transition ease-in-out'>
+            <img  src={image[0]} alt="" />
         </div>
         <p className="pt-3 pb-1 text-sm">{name}</p>
         <p className="font-medium text-sm">{currency}{price}</p>
