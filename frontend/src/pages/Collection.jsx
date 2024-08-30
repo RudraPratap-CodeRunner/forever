199fb0cd34ba48774d2cbf17d075bb2038828e90
RudraPratap-CodeRunner <rp.rudrapratapofficial@gmail.com>
2024-08-30 11:46:48 +0530

    search functionality added

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -6,7 +6,7 @@ import ProductItem from '../components/ProductItem'
 
 const Collection = () => {
   const [showFilter,setShowFilter] = useState(false);
-  const {products} = useContext(ShopContext);
+  const {products,search,showSearch} = useContext(ShopContext);
   const [filterProducts,setFilterProducts] = useState([]);
   const [category,setCategory] = useState([]);
   const [subCategory,setSubCategory] = useState([]);
@@ -29,6 +29,9 @@ const Collection = () => {
   }
   const applyFilter = ()=>{
     let productsCopy = products.slice();
+    if(showSearch && search){
+      productsCopy = productsCopy.filter(item=> item.name.toLowerCase().includes(search.toLowerCase()));
+    }
     if(category.length>0){
       productsCopy = productsCopy.filter(item=>category.includes(item.category));
     }
@@ -55,7 +58,7 @@ const Collection = () => {
 
   useEffect(()=>{
     applyFilter();
-  },[category,subCategory]);
+  },[category,subCategory,search,showSearch]);
 
   useEffect(()=>{
     sortProduct();
