8607a10a210da6268276159b0cc04c5406a09515
RudraPratap-CodeRunner <rp.rudrapratapofficial@gmail.com>
2024-09-03 23:21:05 +0530

    feature for cart total and subtotal added

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
 import { toast } from "react-toastify";
+import {useNavigate} from 'react-router-dom';
 
 export const ShopContext = createContext();
 
@@ -10,7 +11,7 @@ const ShopContextProvider = (props) => {
   const [search, setSearch] = useState("");
   const [showSearch, setShowSearch] = useState(false);
   const [cartItems,setCartItems] = useState({});
-
+  const navigate = useNavigate();
   const addToCart = async (itemId,size)=>{
     if(!size){
       toast.error("Plaese Select Product Size");
@@ -54,6 +55,23 @@ const ShopContextProvider = (props) => {
     setCartItems(cartData);
   }
 
+  const getCartAmount = ()=>{
+    let totalAmount = 0;
+    for(const items in cartItems){
+      let itemInfo = products.find((product)=>product._id===items);
+      for(const item in cartItems[items]){
+        try{
+          if(cartItems[items][item]>0){
+            totalAmount +=itemInfo.price* cartItems[items][item];
+          }
+        }catch(error){
+          
+        }
+      }
+    }
+    return totalAmount;
+  }
+
   const value = {
     products,
     currency,
@@ -62,7 +80,7 @@ const ShopContextProvider = (props) => {
     setSearch,
     showSearch,
     setShowSearch,
-    cartItems,addToCart,getCartCount,updateQuantity
+    cartItems,addToCart,getCartCount,updateQuantity,getCartAmount,navigate
   };
 
   return (
--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -2,9 +2,10 @@ import React, { useContext, useState,useEffect } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import Title from '../components/Title';
 import { assets } from '../assets/assets';
+import CartTotal from '../components/CartTotal';
 
 const Cart = () => {
-  const {products,currency,cartItems,updateQuantity} = useContext(ShopContext);
+  const {products,currency,cartItems,updateQuantity,navigate} = useContext(ShopContext);
   const [cartData,setCartData] = useState([]);
 
   useEffect(() => {
@@ -50,6 +51,14 @@ const Cart = () => {
             )
           })
         }
+      </div>
+      <div className='flex justify-end'>
+        <div className='w-full sm:w-[450px]'>
+          <CartTotal/>
+          <div className="w-full text-end">
+            <button onClick={()=>navigate('/place-order')} className='bg-black text-white text-sm my-8 px-8 py-3'>PROCEED TO CHECKOUT</button>
+          </div>
+        </div>
       </div>
     </div>
   )
