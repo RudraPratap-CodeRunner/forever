ac5b6d5fba57f50bbdb3ad10a88db3339ac0bf07
RudraPratap-CodeRunner <rp.rudrapratapofficial@gmail.com>
2024-09-09 22:34:32 +0530

    spelling errors removed

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -15,7 +15,7 @@ const BestSeller = () => {
         <div className='text-center text-3xl py-8'>
             <Title text1={'BEST'} text2={'SELLERS'} />
             <p className='w-3/4 m-auto text-[16px]  sm:text-sm md:text-base text-gray-600'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. At, veniam?
+                Lorem ipsum dolor sit amet
             </p>
         </div>
         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4  gap-4 gap-y-6">
