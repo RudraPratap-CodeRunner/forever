2d5ac5c9eccc0d396a1462dd360e04d2c26b4fc9
RudraPratap-CodeRunner <rp.rudrapratapofficial@gmail.com>
2024-09-15 16:38:45 +0530

    mongodb.js added

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import cors from 'cors'
 import 'dotenv/config';
+import connectDB from './config/mongodb.js';
 
 const app = express();
 const port = process.env.PORT || 4000;
+connectDB()
 
 // middlewares 
 app.use(express.json());
