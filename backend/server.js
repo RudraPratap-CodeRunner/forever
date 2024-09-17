7e6e63fc59639b981c57a514ab85a4bc6c9abf30
RudraPratap-CodeRunner <rp.rudrapratapofficial@gmail.com>
2024-09-17 08:16:46 +0530

    userController.js and userRoute.js added

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,9 @@ import cors from 'cors'
 import "dotenv/config";
 import {connectDB} from './config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js';
+import userRouter from './routes/userRoute.js';
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 connectDB()
 connectCloudinary()
 
@@ -13,6 +14,8 @@ app.use(express.json());
 app.use(cors());
 
 // api endpoints 
+app.use('/api/user',userRouter)
+
 app.get('/',(req,res)=>{
     res.send("Api Working");
 })
