b3b4cab93a700e48e049b5cd4a55e307113bf7e3
RudraPratap-CodeRunner <rp.rudrapratapofficial@gmail.com>
2024-09-19 15:55:37 +0530

    productModel, ProductController.js and adminLogin function added

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import "dotenv/config";
 import {connectDB} from './config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js';
 import userRouter from './routes/userRoute.js';
+import productRouter from './routes/productRoute.js';
 const app = express();
 const port = process.env.PORT || 4000;
 connectDB()
@@ -15,6 +16,7 @@ app.use(cors());
 
 // api endpoints 
 app.use('/api/user',userRouter)
+app.use('/api/product',productRouter)
 
 app.get('/',(req,res)=>{
     res.send("Api Working");
