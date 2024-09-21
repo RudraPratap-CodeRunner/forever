de06b47fffb2464778d94d445b186bb62de7ca5d
RudraPratap-CodeRunner <rp.rudrapratapofficial@gmail.com>
2024-09-21 23:16:20 +0530

    cartController.js and cartRoute.js added

---
+++ backend/controllers/cartController.js
@@ -0,0 +1,17 @@
+
+// Add product to cart 
+const addToCart = async (req,res)=>{
+
+}
+
+// update cart 
+const updateCart = async (req,res)=>{
+
+}
+
+// get user cart data 
+const getUserCart = async (req,res)=>{
+
+}
+
+export {addToCart,updateCart,getUserCart};
