03720cac1aa03d477051c7fdba85785fa88b343d
RudraPratap-CodeRunner <rp.rudrapratapofficial@gmail.com>
2024-09-22 11:24:15 +0530

    orderRoute.js added

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,36 @@
+
+
+// placing order with cod method  
+const placeOrder = async (req,res)=>{
+
+}
+
+
+// placing order with stripe method  
+const placeOrderStripe = async (req,res)=>{
+    
+}
+
+
+// placing order with Razorpay method  
+const placeOrderRazorpay = async (req,res)=>{
+    
+}
+
+
+// all orders data for admin panel  
+const allOrders = async (req,res)=>{
+    
+}
+
+// user order data frontend   
+const userOrders = async (req,res)=>{
+    
+}
+
+// update order status  from admin panel
+const updateStatus = async (req,res)=>{
+    
+}
+
+export {placeOrder,placeOrderStripe,placeOrderRazorpay,allOrders,updateStatus,userOrders};
