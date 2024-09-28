dfbf845e834dd4220a268a757499905bc7bcd4a9
RudraPratap-CodeRunner <rp.rudrapratapofficial@gmail.com>
2024-09-28 10:32:52 +0530

    stripe payment gateway integrated

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,6 +1,14 @@
+// import { currency } from "../../admin/src/App.jsx";
 import orderModel from "../models/orderModel.js";
 import userModel from "../models/userModel.js";
+import Stripe from 'stripe';
 
+// const global variables 
+const currency = 'inr';
+const deliveryCharge = 10
+
+// stripe gateway initialization 
+const  stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 // placing order with cod method  
 const placeOrder = async (req,res)=>{
@@ -33,7 +41,76 @@ const placeOrder = async (req,res)=>{
 
 // placing order with stripe method  
 const placeOrderStripe = async (req,res)=>{
+    try {
+        const {userId,items,amount,address} = req.body;
+        const {origin} = req.headers;
+
+        const orderData = {
+            userId,
+            items,
+            amount,
+            address,
+            paymentMethod:"Stripe",
+            payment:false,
+            date:Date.now() 
+        }
+
+        const newOrder = new orderModel(orderData);
+        await newOrder.save();
+
+        const line_items = items.map((item)=>({
+            price_data:{
+                currency:currency,
+                product_data:{
+                    name:item.name
+                },
+                unit_amount:item.price*100,
+            },
+            quantity:item.quantity
+        }))
+
+        line_items.push({
+            price_data:{
+                currency:currency,
+                product_data:{
+                    name:'Delivery Charges'
+                },
+                unit_amount: deliveryCharge * 100,
+            },
+            quantity:1
+        })
+
+        const session = await stripe.checkout.sessions.create({
+            success_url:`${origin}/verify?success=true&orderId=${newOrder._id}`,
+            cancel_url:`${origin}/verify?success=false&orderId=${newOrder._id}`,
+            line_items,
+            mode:'payment'
+        })
+
+        res.json({success:true,session_url:session.url})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:error.message});   
+    }
     
+}
+
+// verify payment with stripe 
+const verifyStripe = async (req,res)=>{
+    const {orderId,success,userid} = req.body;
+    try {
+        if(success==="true"){
+            await orderModel.findByIdAndUpdate(orderId,{payment:true});
+            await userModel.findByIdAndUpdate(orderId,{payment:true});
+            res.json({success:true});
+        }else{
+            await orderModel.findByIdAndDelete(orderId);
+            res.json({success:false});
+        }
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:error.message}); 
+    }
 }
 
 
@@ -70,7 +147,14 @@ const userOrders = async (req,res)=>{
 
 // update order status  from admin panel
 const updateStatus = async (req,res)=>{
-    
+    try {
+        const {orderId,status} = req.body;
+        await orderModel.findByIdAndUpdate(orderId,{status});
+        res.json({success:true,message:"Status Updated"});
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:error.message});
+    }
 }
 
-export {placeOrder,placeOrderStripe,placeOrderRazorpay,allOrders,updateStatus,userOrders};
+export {verifyStripe,placeOrder,placeOrderStripe,placeOrderRazorpay,allOrders,updateStatus,userOrders};
--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -61,6 +61,16 @@ const onSubmitHandler  =async (event)=>{
         }
         break;
 
+      case 'stripe':
+        const responseStripe = await axios.post(backendUrl+'/api/order/stripe',orderData,{headers:{token}});
+        if(responseStripe.data.success){
+          const {session_url} = responseStripe.data;
+          window.location.replace(session_url);
+        } else{
+          toast.error(responseStripe.data.message);
+        }
+        break;  
+
       default:
         break;
     }
@@ -104,15 +114,15 @@ const onSubmitHandler  =async (event)=>{
         </div>
         <div className="mt-12">
           <Title text1={'PAYMENT'} text2={'METHOD'} />
-          <div className="flex gap-3 flex-xol lg:flex-row">
-            <div onClick={()=>setMethod('stripe')} className="flex items-center gap-3 border p-2  cursor-pointer">
+          <div className="flex gap-5 flex-col lg:flex-row">
+            <div onClick={()=>setMethod('stripe')} className="flex items-center gap-3 border p-2 pr-10 cursor-pointer">
               <p className={`min-w-3.5 h-3.5 border rounded-full ${method==='stripe' ? 'bg-green-400': ''}`}></p>
               <img className='h-5 ' src={assets.stripe_logo} alt="" />
             </div>
-            <div onClick={()=>setMethod('razorpay')} className="flex items-center gap-3 border p-2  cursor-pointer">
+            {/* <div onClick={()=>setMethod('razorpay')} className="flex items-center gap-3 border p-2  cursor-pointer">
               <p className={`min-w-3.5 h-3.5 border rounded-full ${method==='razorpay' ? 'bg-green-400': ''}`}></p>
               <img className='h-5 ' src={assets.razorpay_logo} alt="" />
-            </div>
+            </div> */}
             <div onClick={()=>setMethod('cod')} className="flex items-center gap-3 border p-2  cursor-pointer">
               <p className={`min-w-3.5 h-3.5 border rounded-full ${method==='cod' ? 'bg-green-400': ''}`}></p>
               <p className='text-gray-500 text-sm font-medium '>CASH ON DELIVERY</p>
