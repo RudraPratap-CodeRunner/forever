3232f322ba60645fdbc0c0121825f2d9286f31db
RudraPratap-CodeRunner <rp.rudrapratapofficial@gmail.com>
2024-10-04 09:20:52 +0530

    admin panel url addded in navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -36,6 +36,10 @@ const Navbar = () => {
           <p>CONTACT</p>
           <hr className="w-11/12 border-none h-[1.5px] bg-gray-700 hidden" />
         </NavLink>
+        <a to="https://forever-admin-beryl.vercel.app" className="flex flex-col items-center gap-1">
+          <p>CONTACT</p>
+          <hr className="w-11/12 border-none h-[1.5px] bg-gray-700 hidden" />
+        </a>
       </ul>
       <div className="flex items-center gap-6">
         <img onClick={()=>{setShowSearch(true);navigate('/collection')}} src={assets.search_icon} className="w-5 cursor-pointer" alt="" />
