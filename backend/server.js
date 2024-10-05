4f6ade2f4c993361a48501904bb192d92d3bc1f1
RudraPratap-CodeRunner <rp.rudrapratapofficial@gmail.com>
2024-10-05 08:44:15 +0530

    Cart page render error removed

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,31 @@
-import express from 'express';
+import express from 'express'
 import cors from 'cors'
-import "dotenv/config";
-import {connectDB} from './config/mongodb.js';
-import connectCloudinary from './config/cloudinary.js';
-import userRouter from './routes/userRoute.js';
-import productRouter from './routes/productRoute.js';
-import cartRouter from './routes/cartRoute.js';
-import orderRouter from './routes/orderRoute.js';
+import 'dotenv/config'
+import connectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
+import userRouter from './routes/userRoute.js'
+import productRouter from './routes/productRoute.js'
+import cartRouter from './routes/cartRoute.js'
+import orderRouter from './routes/orderRoute.js'
 
-
-const app = express();
-const port = process.env.PORT || 4000;
+// App Config
+const app = express()
+const port = process.env.PORT || 4000
 connectDB()
 connectCloudinary()
 
-// middlewares 
-app.use(express.json());
-app.use(cors());
+// middlewares
+app.use(express.json())
+app.use(cors())
 
-// api endpoints 
+// api endpoints
 app.use('/api/user',userRouter)
 app.use('/api/product',productRouter)
 app.use('/api/cart',cartRouter)
 app.use('/api/order',orderRouter)
 
 app.get('/',(req,res)=>{
-    res.send("Api Working");
+    res.send("API Working")
 })
-app.listen(port,()=>{
-    console.log(`server running on http://localhost:${port}`);
-})
+
+app.listen(port, ()=> console.log('Server started on PORT : '+ port))
